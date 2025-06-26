0087241e9cd2d3d13c0e6358a37633e7af963270
CodeRamana <mailgo2sr@gmail.com>
2025-06-26 17:02:23 +0530

    Fixed Image issues

--- src/index.js
+++ src/index.js
@@ -179,7 +179,7 @@ const setDashboard = (filterTabs, transactions) => {
     balance.textContent = `Rs: ${(reduced.Income ?? 0) - (reduced.Expense ?? 0)}`;
 
     //indicating the net balance in image dynamically.
-    document.getElementById("indicator").src = ((reduced.Income  ?? 0) > (reduced.Expense ?? 0)) ? "/public/rocket.png" : "/public/cartoon.jpg";
+    document.getElementById("indicator").src = ((reduced.Income  ?? 0) > (reduced.Expense ?? 0)) ? "public/rocket.png" : "public/cartoon.jpg";
 
     // filtering the transactions data from the buttons in Dashboard
     const dashBoardBtn = document.querySelectorAll('#dashboard > div > div > a');
